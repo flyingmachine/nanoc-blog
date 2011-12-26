d66993c51a8fc0dc5d332a58ebf7669f699b1439
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2011-12-26 13:00:41 -0500

    more integration

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -6,10 +6,6 @@ module CustomHelpers
     Time.parse(date).strftime("%d %B %Y")    
   end
 
-  def read_more(article)
-    
-  end
-
   def link_to(text, item, html_class = nil)
     "<a href='#{item.path}' class='#{html_class}'>#{text}</a>"
   end
