a5622d065430db1bbf1939dfe4a3a8cb724b9747
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2012-10-03 08:34:42 -0400

    un submodulize it

---
+++ content/assets/source/clojure/refactoring-to-datamappers/refactored/db.clj
@@ -0,0 +1,48 @@
+(ns arenaverse.data-mappers.db)
+
+(defmacro add-db-reqs []
+  '(do
+     (require 'monger.collection)
+     (import 'org.bson.types.ObjectId)))
+
+;; TODO ~' Insanity! #cthulhu
+;; These macros are meant to infect the namespace with functions. Why
+;; would I want to do this? Should I take heed of the fact that
+;; Clojure really doesn't want me to?
+
+;; I wrote these fucntions to avoid having to write collection-name
+;; all over the place
+(defmacro add-db-fns [collection-name]
+  `(let [collection-name# ~collection-name]
+    (def ~'db-destroy (partial monger.collection/remove-by-id collection-name#))     
+    (def ~'db-one (partial monger.collection/find-one-as-map collection-name#))
+    (def ~'db-one-by-id (partial monger.collection/find-map-by-id collection-name#))
+    (def ~'db-all (partial monger.collection/find-maps collection-name#))
+    (def ~'db-insert (partial monger.collection/insert collection-name#))
+    (def ~'db-update-by-id (partial monger.collection/update-by-id collection-name#))
+    (def ~'db-update (partial monger.collection/update collection-name#))))
+
+;; These methods are meant to generate the representations which
+;; non-db parts of the code will use. They all convert ObjectId's to
+;; strings because no other part of the system should care about ObjectId's
+(defmacro add-finder-fns []
+  '(do
+     ;; TODO this doesn't feel like it belongs here. It's a helper
+     ;; method. But this macro approach is infecting everything!
+     (defn idstr [record]
+       (.toString (:_id record)))
+     
+     (defn object-id->idstr [record]
+       (assoc record :_id (idstr record)))
+
+     ;; TODO I don't like mapping in the all fn, feels wasteful.
+     (defn all [& [query-doc]]
+       (map object-id->idstr (db-all query-doc)))
+     
+     (defn one [& [query-doc]]
+       (if-let [r (db-one query-doc)]
+         (object-id->idstr r)))
+     
+     (defn one-by-id [_id]
+       (if-let [r (db-one-by-id (ObjectId. _id))]
+         (object-id->idstr r)))))
---
+++ content/assets/source/clojure/refactoring-to-datamappers/refactored/fighter.clj
@@ -0,0 +1,146 @@
+(ns arenaverse.data-mappers.fighter
+  (:require [arenaverse.data-mappers.db :as db]
+            [arenaverse.lib.aws.s3 :as s3]
+            [arenaverse.config :as config])
+
+  (:import [org.bson.types ObjectId]
+           [org.imgscalr Scalr Scalr$Method Scalr$Mode]
+           [javax.imageio ImageIO]
+           [java.awt.image BufferedImageOp]
+           [java.awt.image BufferedImage]
+           [org.apache.commons.io FilenameUtils]))
+
+(declare one-by-id)
+
+(db/add-db-reqs)
+(let [collection-name "fighters"]
+  (db/add-db-fns collection-name)
+  (db/add-finder-fns))
+
+(def *image-versions [["card" 192]
+                      ["listing" 64]
+                      ["battle" 432 638]])
+
+;;----
+;; Images
+;;----
+(defn image-relative-path [version {:keys [_id image-extension]}]
+  (str "fighters/" _id "/" version "." image-extension))
+
+(defn image-path [version record]
+  (str "/" (image-relative-path version record)))
+
+;; TODO not hardcode this
+(defn- bucket-name []
+  (str "arenaverse-" (name config/env)))
+
+(defn amazon-image-path [version record]
+  (str "https://s3.amazonaws.com/" (bucket-name) (image-path version record)))
+
+(defn- normalize-image-extension [extension]
+  (clojure.string/replace extension "jpeg" "jpg"))
+
+(defn- image-fields [object-id image-extension]
+  {:_id object-id
+   :image-extension (normalize-image-extension image-extension)})
+
+(defn- resize
+  ([image box]
+     (resize image box box))
+  ([image target-width target-height]
+     (let [width  (.getWidth image)
+           height (.getHeight image)
+           fit (if (> (/ target-width width) (/ target-height height)) Scalr$Mode/FIT_TO_HEIGHT Scalr$Mode/FIT_TO_WIDTH)]
+       (Scalr/resize image Scalr$Method/ULTRA_QUALITY fit target-width target-height (into-array BufferedImageOp [])))))
+
+(defn- buffered-image->input-stream [buffered-image extension]
+  (let [os (java.io.ByteArrayOutputStream.)]
+    (ImageIO/write buffered-image extension os)
+    (java.io.ByteArrayInputStream. (.toByteArray os))))
+
+(defn- input->image-extension [input]
+  (FilenameUtils/getExtension (:filename (:file input))))
+
+(defn- image-uploaded? [input]
+  (not (= 0 (:size (:file input)))))
+
+(defn- input->images [input]
+  (let [file-upload   (:file input)
+        original-file (:tempfile file-upload)
+        content-type  (:content-type file-upload)
+        original-image {:version "original"
+                        :file original-file
+                        :content-type content-type}
+        image-extension (normalize-image-extension (input->image-extension input))
+        buff-img (ImageIO/read original-file)]
+    
+    ;; TODO make this a lazy seq
+    (conj (map (fn [[version & dim]]
+                 {:version version
+                  :file (buffered-image->input-stream
+                         (apply resize (cons buff-img dim))
+                         image-extension)
+                  :content-type content-type})
+               *image-versions)
+          original-image)))
+
+(defn- store-image [image, record]
+  (s3/put-object config/*aws-credentials*
+                 (bucket-name)
+                 (image-relative-path (:version image) record)
+                 (:file image)
+                 {:content-type (:content-type image)}
+                 #(.withCannedAcl % com.amazonaws.services.s3.model.CannedAccessControlList/PublicRead)))
+
+
+(defn- store-images [input db-fields]
+  (future
+    (doseq [image (input->images input)]
+      (store-image image db-fields))))
+
+(defn- create-input->db-fields [input]
+  (let [object-id (ObjectId.)]
+    (merge
+     (dissoc input :file)
+     (image-fields object-id (input->image-extension input)))))
+
+(defn- update-input->db-fields [input]
+  (let [object-id (ObjectId. (:_id input))
+        record (db-one-by-id object-id)
+        ;; ensure that the user doesn't alter the arena id
+        ;; and that image-extension isn't overwritten when no file is present
+        db-fields (merge
+                   (select-keys record [:image-extension])
+                   (dissoc input :_id :file)
+                   (select-keys record [:arena-id]))]
+    (if (image-uploaded? input)
+      (merge db-fields (image-fields object-id (input->image-extension input)))
+      db-fields)))
+
+(defn create [input]
+  (let [db-fields (create-input->db-fields input)]
+    (db-insert db-fields)
+    (when (image-uploaded? input) (store-images input db-fields))
+    db-fields))
+
+;; this is weird... i remove the object id in the ->db-fields method,
+;; then add it back again
+(defn update [input]
+  (let [db-fields (update-input->db-fields input)
+        object-id (ObjectId. (:_id input))
+        record    (merge db-fields {:_id object-id})]
+    (db-update-by-id object-id db-fields)
+    (when (image-uploaded? input) (store-images input db-fields))
+    db-fields))
+
+;; TODO query S3 first to avoid missing any images if i.e. image
+;; version names change
+(defn- delete-images [record]
+  (doseq [[vname] (conj *image-versions ["original"])]
+    (s3/delete-object config/*aws-credentials* (bucket-name) (image-relative-path vname record))))
+
+(defn destroy [_id]
+  (let [object-id (ObjectId. _id)
+        record (db-one-by-id object-id)]
+    (delete-images record)
+    (db-destroy object-id)))
