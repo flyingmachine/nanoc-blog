cdfd28db3bc964e0abd1a3b23cb00ae3b27c5a81
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2012-11-19 21:00:52 -0500

    restructuring css

--- compass/config.rb
+++ compass/config.rb
@@ -1,8 +1,8 @@
+require 'susy'
 http_path    = "/" 
-project_path = "." 
-css_dir      = "output/assets/style" 
-sass_dir     = "content/assets/stylesheets" 
-images_dir   = "output/assets/images"
+css_dir      = "../output/stylesheets" 
+sass_dir     = "../content/assets/stylesheets" 
+images_dir   = "../output/assets/images"
 
 # when using SCSS:
 sass_options = {
